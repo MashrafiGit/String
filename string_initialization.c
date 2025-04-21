454082612db6b1d7f899dc510ddd66ff6b340829
Masrafi404 <mdmasrafi902@gmail.com>
2025-04-21 13:05:33 +0600

    string initialization

---
+++ string_initialization.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    // we don't need to declare '\0' = null, just need to empty one step
+    // char s[9] = {'M', 'a', 's', 'h', 'r', 'a', 'f', 'i'};
+
+    // shortcut easy way with space
+    char s[19] = "Ali Hasan Mashrafi";
+    printf("%s", s);
+
+    return 0;
+}
