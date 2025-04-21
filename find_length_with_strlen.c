f7441316d5d6847d58dbb2e9a6bf0c4d0342f42a
Masrafi404 <mdmasrafi902@gmail.com>
2025-04-21 13:40:00 +0600

    findout length with strlen

---
+++ find_length_with_strlen.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[50];
+    scanf("%s", s);
+
+    // int length = strlen(s);
+    
+    // sortcut to print length
+    printf("%d", strlen(s));
+
+    return 0;
+}
